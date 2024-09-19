fdd970c3869897878058e1ffab3941097b71995b
DenisYamalov <nokiden91@gmail.com>
2024-09-19 09:56:00 +0300

    fix: database init and populate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO USERS (name, email, password, calories_per_day)
-VALUES ('User', 'user@yandex.ru', '{noop}password', 2005),
-       ('Admin', 'admin@gmail.com', '{noop}admin', 1900),
-       ('Guest', 'guest@gmail.com', '{noop}guest', 2000);
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', '{noop}password'),
+       ('Admin', 'admin@gmail.com', '{noop}admin'),
+       ('Guest', 'guest@gmail.com', '{noop}guest');
 
-INSERT INTO USER_ROLE (role, user_id)
+INSERT INTO user_role (role, user_id)
 VALUES ('USER', 1),
        ('ADMIN', 2),
        ('USER', 2);
