5b00462b84be08a1d901a3f84af2768dd7e63614
DenisYamalov <nokiden91@gmail.com>
2024-09-20 08:01:12 +0300

    refactor: remove BaseNamedRepository

--- src/main/java/ru/lunchvoting/user/repository/DishRepository.java
+++ src/main/java/ru/lunchvoting/user/repository/DishRepository.java
@@ -1,9 +1,9 @@
 package ru.lunchvoting.user.repository;
 
 import org.springframework.transaction.annotation.Transactional;
-import ru.lunchvoting.common.BaseNamedRepository;
+import ru.lunchvoting.common.BaseRepository;
 import ru.lunchvoting.user.model.Dish;
 
 @Transactional(readOnly = true)
-public interface DishRepository extends BaseNamedRepository<Dish> {
+public interface DishRepository extends BaseRepository<Dish> {
 }
--- src/main/java/ru/lunchvoting/user/repository/RestaurantRepository.java
+++ src/main/java/ru/lunchvoting/user/repository/RestaurantRepository.java
@@ -1,9 +1,12 @@
 package ru.lunchvoting.user.repository;
 
 import org.springframework.transaction.annotation.Transactional;
-import ru.lunchvoting.common.BaseNamedRepository;
+import ru.lunchvoting.common.BaseRepository;
 import ru.lunchvoting.user.model.Restaurant;
 
+import java.util.Optional;
+
 @Transactional(readOnly = true)
-public interface RestaurantRepository extends BaseNamedRepository<Restaurant> {
+public interface RestaurantRepository extends BaseRepository<Restaurant> {
+    Optional<Restaurant> findByName(String name);
 }
